356fb7023e20d491eddd401baf88bf11c6b0dbfa
LuisAndaur <andaurluisalejandro@gmail.com>
2024-08-23 00:32:01 -0300

    Trabajando sobre API clientes, se agregó exc not found. Se está modificando los response

--- src/core/src/main/java/com/tienda/core/controllers/ClienteRestController.java
+++ src/core/src/main/java/com/tienda/core/controllers/ClienteRestController.java
@@ -1,15 +1,18 @@
 package com.tienda.core.controllers;
 
 import com.tienda.core.domain.Cliente;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import com.tienda.core.exceptions.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteRestController {
 
     private List<Cliente> clientes = new ArrayList<>(Arrays.asList(
@@ -19,16 +22,55 @@ public class ClienteRestController {
         )
     );
 
-    @GetMapping("/clientes")
-    public List<Cliente> getClientes(){
-        return clientes;
+    @GetMapping
+    public ResponseEntity<List<?>> getClientes(){
+
+        return ResponseEntity.ok(clientes);
     }
 
-    @GetMapping("/clientes/{userName}")
-    public Cliente getClientes(@PathVariable String userName){
+    @GetMapping("/{userName}")
+    public ResponseEntity<?> getClientes(@PathVariable String userName){
 
-        return clientes.stream().
-                filter(cliente -> cliente.getUsername().equalsIgnoreCase(userName)).
+        return clientes.stream()
+                .filter(c -> c.getUsername().equalsIgnoreCase(userName))
+                .findFirst()
+                .map(ResponseEntity::ok)
+                .orElseThrow(() -> new ResourceNotFoundException("Cliente " + userName + " no encontrado."));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> altaCliente(@RequestBody Cliente cliente){
+        clientes.add(cliente);
+
+        //Obteniendo URL del servicio
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{userName}")
+                .buildAndExpand(cliente.getUsername())
+                .toUri();
+
+        return ResponseEntity.created(location).body(cliente);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> modificacionCliente(@RequestBody Cliente cliente){
+        Cliente clienteEncontrado = clientes.stream().
+                filter(c -> c.getUsername().equalsIgnoreCase(cliente.getUsername())).
                 findFirst().orElseThrow();
+        clienteEncontrado.setPassword(cliente.getPassword());
+        clienteEncontrado.setNombre(cliente.getNombre());
+
+        return ResponseEntity.ok(clienteEncontrado);
+    }
+
+    @DeleteMapping("/{userName}")
+    public ResponseEntity deleteCliente(@PathVariable String userName){
+        Cliente clienteEncontrado = clientes.stream().
+                filter(c -> c.getUsername().equalsIgnoreCase(userName)).
+                findFirst().orElseThrow();
+
+        clientes.remove(clienteEncontrado);
+
+        return ResponseEntity.noContent().build();
     }
 }
